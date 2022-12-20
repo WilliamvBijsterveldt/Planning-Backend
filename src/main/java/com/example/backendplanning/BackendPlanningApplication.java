d1d920ce5a25e5a93d74b04c73d1723feb0cfd34
WilliamvBijsterveldt <wvanbijsterveldt@gmail.com>
2022-12-20 18:26:11 +0100

    Tried adding Websocket

--- src/main/java/com/example/backendplanning/BackendPlanningApplication.java
+++ src/main/java/com/example/backendplanning/BackendPlanningApplication.java
@@ -2,9 +2,10 @@ package com.example.backendplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendPlanningApplication {
+public class BackendPlanningApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendPlanningApplication.class, args);
